16e2995cc06943d83ddb6b9503d870273bf48ed7
kais <ing.amiri.kais@gmail.com>
2021-05-27 08:02:36 +0100

    third commit

--- src/profile/myInformations.js
+++ src/profile/myInformations.js
@@ -2,7 +2,7 @@ export const myinfo = {
   name: "Kais",
   lastName: "Amiri",
   profession: "full stack developer",
-  phoneNumber: 99999999,
+  phoneNumber: "99999999",
   adress: "Ben Arous, Tunisie",
   bio: "I am student in gomycode",
 };
